bf38bd3159ade53f9118860e1fff1fb66f7d02cc
swante <swante@gmail.com>
2024-05-06 19:10:50 +0200

    a

--- vnext/graph.js
+++ vnext/graph.js
@@ -8,9 +8,7 @@ a;aai-winter;6;10;Permanent AI winter
 i;ireach-catastrophic-potential;10;8;Frontier AI reaches capabilities with catastrophic potential
 n;ncatastrophe;10;10;pre-AGI catastrophe occurs (e.g. AI misuse via cyber- or bio-weapons)?
 i;ccatastrophy-occurs;11;12;pre-AGI catastrophe does occur!
-n;ntry-stop2;12;10;Humanity permanently fully stops frontier AI development (after catastrophe)?
-n;nenforce-slow;12;14;Humanity enforces slow/careful AI development globally?
-g;gagi-utopia;12;16;(Slightly delayed) AGI utopia
+n;ntry-stop2;12;14;Humanity permanently fully stops frontier AI development (after catastrophe)?
 i;iagi-exists;10;14;Research towards AGI continues. First AGI exists
 n;nagi-transformative;8;12;Is AGI transformative?
 a;anon-transformative-agi;6;12;Powerful, but not transformative AGI
@@ -18,6 +16,7 @@ n;nalignment-theory;8;14;Do we have a practical solution for AGI alignment?
 n;nimplement-alignment1;6;14;Will the first AGI lab implement alignment correctly?
 n;nprevent-other-agis;4;14;Will the first AGI prevent other AGIs from appearing?
 n;nimplement-alignment2;6;16;Will all other AGI labs implement alignment correctly?
+i;imultipolar;7;17.5;Multiple conflicting AGIs exist
 i;ialigned-agi;4;16;Aligned AGI
 n;nwhole-humanity-aligned;4;18;Is AGI aligned with humanity's interests as a whole?
 g;gagi-utopia2;4;20;AGI utopia
@@ -26,7 +25,7 @@ n;nagi-protects;8;20;Does AGI protect its users?
 e;estupid-xrisk;8;22;Accidental human extinction
 n;ncontrollers-good;6;20;Do AGI controllers have good intentions?
 e;eauthoritarian-dystopia;6;22;Authoritarian dystopia
-i;imisaligned-agi;8;16;Misaligned AGI
+i;imisaligned-agi;8;16;(At least one)|Misaligned AGI
 n;nfigure-misaligned;8;18;Do we figure out right away that it's misaligned?
 n;nturn-off;10;18;Can we easily turn it off?
 a;asecond-chance;12;18;We get a second chance at building AGI
@@ -50,9 +49,7 @@ y;ncapabilities-plateau;aai-winter
 y;ncatastrophe;ccatastrophy-occurs
 -;ccatastrophy-occurs;ntry-stop2
 y;ntry-stop2;adune
-n;ntry-stop2;nenforce-slow
-y;nenforce-slow;gagi-utopia
-n;nenforce-slow;iagi-exists
+n;ntry-stop2;iagi-exists
 n;ncatastrophe;iagi-exists
 -;iagi-exists;nagi-transformative
 y;nagi-transformative;nalignment-theory
@@ -63,7 +60,8 @@ n;nimplement-alignment1;imisaligned-agi
 y;nimplement-alignment1;nprevent-other-agis
 n;nprevent-other-agis;nimplement-alignment2
 y;nprevent-other-agis;ialigned-agi
-n;nimplement-alignment2;imisaligned-agi
+n;nimplement-alignment2;imultipolar
+-;imultipolar;imisaligned-agi
 y;nimplement-alignment2;ialigned-agi
 -;ialigned-agi;nwhole-humanity-aligned
 n;nwhole-humanity-aligned;ncontrollers-thoughtful
